d4b5d16fb4746735e83d4630fa6eeea927e43249
sylviah <sylviah.rutto@gmail.com>
2019-08-14 12:00:01 +0300

    added changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,45 @@
+from flask import render_template,request,redirect,url_for,abort
+from flask_login import login_required,current_user
+from . import main
+from .. import db,photos
+from ..models import User,,Results
+from .forms import UpdateProfile,BlogsForm,CommentsForm
+
+
+
+
+@main.route('/')
+def vote():
+    pw=""
+    return render_template('user_list.html')
+    return pw
+
+@main.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
+
+@main.route('/nps_vote', methods=['POST'])
+def post_user():
+    vote = Votes(request.form['hg'], request.form['hb'])
+    db.session.add(vote)
+    db.session.commit()
+    return redirect(url_for('thanks'))
+
+
+@main.route('/nps_results')
+@login_required
+def results():
+        ngigi=Votes.query.filter(Votes.hb == "ngigi").count()
+        sylviah=Votes.query.filter(Votes.hb == "sylviah").count()
+        michelle=Votes.query.filter(Votes.hg == "michelle").count()
+        mercy=Votes.query.filter(Votes.hg == "mercy").count()
+
+        print("Total Voters",(sneha + neha))
+
+        results={"Ngigi":ngigi,"Sylviah":sylviah,"Michellle":michelle,"Mercy":mercy}
+
+
+        return render_template("results.html", results=results)
+
 
