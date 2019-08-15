d02e4719000b68b363e18b42c08c3d939687856a
Michelle Mukami <you@example.com>
2019-08-15 12:48:42 +0300

    chnages

--- config.py
+++ config.py
@@ -1,32 +1,5 @@
 import os
 
-class Config:
-
-   
-    SECRET_KEY = 'kamikaze'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/kami'
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-class ProdConfig(Config):
-     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/vote_test'
-   
-
-class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/vote'
-    
-    DEBUG = True
-
-config_options = {
-'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
-}
+SECRET_KEY = 'top-secret'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
