4d0a9e2d15ae1286722864c72882412ec1c09aa9
macymuhia <macymuhia@gmail.com>
2019-08-15 05:57:35 +0300

    change

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from ..email import mail_message
 # views
 @main.route("/", methods=['GET', 'POST'])
 def index():
-    users = ['michellemukami.g@gmail.com', 'macymuhia@gmail.com', 'sylviah.rutto@gmail.com']
+    users = ['macymuhia@gmail.com', 'mercy8muhia@gmail.com', 'maretekent@gmail.com']
     for user in range(len(users)):
         mail_message('iVote', 'email/welcome_user', users[user])
     return render_template('index.html')
