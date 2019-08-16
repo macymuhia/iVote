210b8b7b33845fc01ce0438f70b9b5a74cddc2f7
Michelle Mukami <you@example.com>
2019-08-16 07:54:56 +0300

    changes

---
+++ app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+simple = SimpleMDE()                  
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    # configure UploadSet
+   
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    simple.init_app(app)
+    login_manager.init_app(app)
+
+    mail.init_app(app)
+    # Will add the views and forms
+    # Registering the blueprint
+    # from .main import main as main_blueprint
+    from .main import main as p
+    app.register_blueprint(p)
+
+    # setting config
+    # from .request import configure_request
+    # configure_request(app)
+    
+    
+
+    return app
