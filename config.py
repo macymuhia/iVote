1db736d7e3e217e55405266de3436a6df767f3fb
Michelle Mukami <you@example.com>
2019-08-16 08:07:01 +0300

    deployments

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
 
     DEBUG = True
 class ProdConfig(Config):
-   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/sin'
+   SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL')
 class TestConfig(Config):
    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/sin'
 
